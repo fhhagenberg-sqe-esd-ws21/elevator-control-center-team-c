35b74a6d70efb174392c097909becc9a9bfd3cfd
Anita Michlits <s2010567020@fhooe.at>
2021-12-21 17:59:53 +0100

    added test for remaining methods

--- src/test/java/at/fhhagenberg/sqe/ecc/model/ElevatorControllerTest.java
+++ src/test/java/at/fhhagenberg/sqe/ecc/model/ElevatorControllerTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sqelevator.IElevator;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,7 +24,6 @@ public class ElevatorControllerTest {
     private ElevatorController elevatorController;
 
     private final double factorFeetToMeter = 0.3048;
-    private final double factorPoundToKg = 0.45359237;
 
     @BeforeEach
     void Setup(){
@@ -56,7 +54,8 @@ public class ElevatorControllerTest {
     @Test
     void testGetWeightConversion() throws RemoteException {
         when(mockedIElevator.getElevatorWeight(0)).thenReturn(1);
-        assertEquals(1*factorPoundToKg, elevatorController.getElevatorWeight(0));
+        double factorPoundToKg = 0.45359237;
+        assertEquals(1* factorPoundToKg, elevatorController.getElevatorWeight(0));
     }
 
     @Test
@@ -124,4 +123,100 @@ public class ElevatorControllerTest {
         when(mockedIElevator.getElevatorDoorStatus(0)).thenReturn(ELEVATOR_DOORS_CLOSING);
         assertEquals(IElevatorController.DoorState.CLOSING, elevatorController.getElevatorDoorStatus(0));
     }
+
+
+    // additional tests for code coverage
+
+    @Test
+    void testGetElevatorButton() throws RemoteException {
+        when(mockedIElevator.getElevatorButton(0, 1)).thenReturn(true);
+        when(mockedIElevator.getElevatorButton(0, 2)).thenReturn(false);
+
+        assertTrue(elevatorController.getElevatorButton(0, 1));
+        assertFalse(elevatorController.getElevatorButton(0, 2));
+    }
+
+    @Test
+    void testGetCurrentFloor() throws RemoteException{
+        when(mockedIElevator.getElevatorFloor(0)).thenReturn(0);
+        when(mockedIElevator.getElevatorFloor(1)).thenReturn(2);
+
+        assertEquals(0, elevatorController.getElevatorFloor(0));
+        assertEquals(2, elevatorController.getElevatorFloor(1));
+    }
+
+    @Test
+    void testGetNumberOfElevators() throws RemoteException{
+        when(mockedIElevator.getElevatorNum()).thenReturn(2);
+        assertEquals(2, elevatorController.getElevatorNum());
+    }
+
+    @Test
+    void testGetCapacity() throws RemoteException{
+        when(mockedIElevator.getElevatorCapacity(0)).thenReturn(2);
+        when(mockedIElevator.getElevatorCapacity(1)).thenReturn(0);
+
+        assertEquals(2, elevatorController.getElevatorCapacity(0));
+        assertEquals(0, elevatorController.getElevatorCapacity(1));
+    }
+
+    @Test
+    void testGetFloorButtonDown() throws RemoteException{
+        when(mockedIElevator.getFloorButtonDown(0)).thenReturn(true);
+        when(mockedIElevator.getFloorButtonDown(1)).thenReturn(false);
+
+        assertTrue(elevatorController.getFloorButtonDown(0));
+        assertFalse(elevatorController.getFloorButtonDown(1));
+    }
+
+    @Test
+    void testGetFloorButtonUp() throws RemoteException{
+        when(mockedIElevator.getFloorButtonUp(0)).thenReturn(true);
+        when(mockedIElevator.getFloorButtonUp(1)).thenReturn(false);
+
+        assertTrue(elevatorController.getFloorButtonUp(0));
+        assertFalse(elevatorController.getFloorButtonUp(1));
+    }
+
+    @Test
+    void testGetNumberOfFloors() throws RemoteException{
+        when(mockedIElevator.getFloorNum()).thenReturn(0);
+        assertEquals(0, elevatorController.getFloorNum());
+    }
+
+    @Test
+    void testGetServicesFloor() throws RemoteException{
+        when(mockedIElevator.getServicesFloors(0, 0)).thenReturn(true);
+        when(mockedIElevator.getServicesFloors(0, 2)).thenReturn(false);
+
+        assertTrue(elevatorController.getServicesFloors(0,0));
+        assertFalse(elevatorController.getServicesFloors(0,2));
+    }
+
+    @Test
+    void testGetTarget() throws RemoteException{
+        when(mockedIElevator.getTarget(0)).thenReturn(0);
+        assertEquals(0, elevatorController.getTarget(0));
+    }
+
+    @Test
+    void testSetServicesFloor() throws RemoteException{
+        elevatorController.setServicesFloors(0, 2, true);
+        verify(mockedIElevator).setServicesFloors(0, 2, true);
+    }
+
+    @Test
+    void testSetTarget() throws RemoteException{
+        elevatorController.setTarget(0, 2);
+        verify(mockedIElevator).setTarget(0, 2);
+    }
+
+    @Test
+    void testGetClockTick() throws RemoteException{
+        when(mockedIElevator.getClockTick()).thenReturn(10L);
+        assertEquals(10L, elevatorController.getClockTick());
+    }
 }
+
+
+
