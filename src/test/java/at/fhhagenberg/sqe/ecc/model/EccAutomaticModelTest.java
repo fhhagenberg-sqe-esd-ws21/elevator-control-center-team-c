4196d9086576a9be0cfa406e075c29f068be6301
Anita Michlits <s2010567020@fhooe.at>
2022-01-19 00:40:34 +0100

    added GetRequests tests - failing

--- src/test/java/at/fhhagenberg/sqe/ecc/model/EccAutomaticModelTest.java
+++ src/test/java/at/fhhagenberg/sqe/ecc/model/EccAutomaticModelTest.java
@@ -45,14 +45,14 @@ class EccAutomaticModelTest {
     }
 
     @Test
-    void testStartRunStop() throws ExecutionException, InterruptedException, RemoteException {
+    void testStartRunStop() throws InterruptedException {
         model.getElevator(0).setCurrentFloor(0);
         model.getElevator(0).setDirection(CommittedDirection.UP);
         model.getElevator(0).setButtonPressed(2, true);
 
         automaticMode.StartAutomaticMode();
         assertTrue(automaticMode.getAutomaticModeRunning());
-
+        // unnecessary
         var future = automaticMode.Run();
         Thread.sleep(future.getDelay(TimeUnit.MILLISECONDS) + 10);
 
@@ -62,7 +62,19 @@ class EccAutomaticModelTest {
     }
 
     @Test
-    void testGetNextStopRequest(){
+    void testRunWithoutStarting() throws ExecutionException, InterruptedException, RemoteException {
+        model.getElevator(0).setCurrentFloor(0);
+        model.getElevator(0).setDirection(CommittedDirection.UP);
+        model.getElevator(0).setButtonPressed(2, true);
+
+        var future = automaticMode.Run();
+        Thread.sleep(future.getDelay(TimeUnit.MILLISECONDS) + 10);
+
+        assertEquals(0, model.getElevator(0).getTargetFloor());
+    }
+
+    @Test
+    void testGetNextStopRequestStayingUP(){
         model.getElevator(0).setCurrentFloor(0);
         model.getElevator(0).setDirection(CommittedDirection.UP);
         model.getElevator(0).setButtonPressed(2, true);
@@ -71,11 +83,67 @@ class EccAutomaticModelTest {
     }
 
     @Test
-    void testGetNextCallRequest(){
+    void testGetNextStopRequestGoingDOWN(){
+        model.getElevator(0).setCurrentFloor(1);
+        model.getElevator(0).setDirection(CommittedDirection.UP);
+        model.getElevator(0).setButtonPressed(0, true);
+
+        assertEquals(0, automaticMode.GetNextStopRequest(0));
+        assertEquals(0, model.getElevator(0).getTargetFloor());
+        assertEquals(CommittedDirection.DOWN, model.getElevator(0).getDirection());
+    }
+
+
+    @Test
+    void testGetNextCallRequestUP(){
         model.getElevator(0).setCurrentFloor(0);
         model.getElevator(0).setDirection(CommittedDirection.UP);
         model.getFloor(1).setUpButtonPressed(true);
 
         assertEquals(1, automaticMode.GetNextCallRequest(0));
+    }
+
+    @Test
+    void testGetNextCallRequestGoingDOWN(){
+        model.getElevator(0).setCurrentFloor(2);
+        model.getElevator(0).setDirection(CommittedDirection.UP);
+        model.getFloor(1).setUpButtonPressed(true);
+
+        assertEquals(1, automaticMode.GetNextStopRequest(0));
+        assertEquals(1, model.getElevator(0).getTargetFloor());
+        assertEquals(CommittedDirection.DOWN, model.getElevator(0).getDirection());
+    }
+
+    @Test
+    void testGetNextMixedStartingUNCOMMITTEDChoosingCALLGoingDOWN(){
+        model.getElevator(0).setDirection(CommittedDirection.UNCOMMITTED);
+        model.getElevator(0).setCurrentFloor(2);
+        model.getFloor(1).setUpButtonPressed(true);
+        model.getElevator(0).setButtonPressed(0, true);
+
+        assertEquals(1, model.getElevator(0).getTargetFloor());
+        assertEquals(CommittedDirection.DOWN, model.getElevator(0).getDirection());
+    }
+
+    @Test
+    void testGetNextMixedStartingUNCOMMITTEDChoosingSTOPGoingDOWN(){
+        model.getElevator(0).setDirection(CommittedDirection.UNCOMMITTED);
+        model.getElevator(0).setCurrentFloor(1);
+        model.getFloor(2).setUpButtonPressed(true);
+        model.getElevator(0).setButtonPressed(0, true);
+
+        assertEquals(0, model.getElevator(0).getTargetFloor());
+        assertEquals(CommittedDirection.DOWN, model.getElevator(0).getDirection());
+    }
+
+    @Test
+    void testGetNextMixedStartingUNCOMMITTEDChoosingSTOPGoingUP(){
+        model.getElevator(0).setDirection(CommittedDirection.UNCOMMITTED);
+        model.getElevator(0).setCurrentFloor(1);
+        model.getFloor(0).setUpButtonPressed(true);
+        model.getElevator(2).setButtonPressed(0, true);
+
+        assertEquals(2, model.getElevator(0).getTargetFloor());
+        assertEquals(CommittedDirection.UP, model.getElevator(0).getDirection());
     }
 }
