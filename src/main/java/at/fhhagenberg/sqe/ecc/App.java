39854fa71f6d37114a4ec17c5193d8922946b115
MikeEdu <s2010567027@fhooe.at>
2022-01-18 23:31:44 +0100

    Add controller to app

--- src/main/java/at/fhhagenberg/sqe/ecc/App.java
+++ src/main/java/at/fhhagenberg/sqe/ecc/App.java
@@ -14,44 +14,34 @@ import javafx.stage.Stage;
 
 public class App extends Application{
 
-	public static void main(String[] args) {
-		launch(args);
+	EccController controller;
+
+	public App()
+	{
+		controller = new EccController();
 	}
 	 
 	@Override
 	public void start(Stage stage) {
-		
-	    // init model
-	    var elevCnt = 8;
-     	var floorCnt = 12;
-     	var elevators = new ArrayList<Elevator>();
-    	for(int i = 0; i < elevCnt; i++)
-        {
-    		var elev = new Elevator(floorCnt, 10);
-    		elev.setDoorState(DoorState.CLOSED);
-    		elev.setButtonPressed(floorCnt-1, true);
-    		elevators.add(elev);
-        }
-        var floors = new ArrayList<Floor>();
-        for(int i = 0; i < floorCnt; i++)
-        {
-        	var floor = new Floor();
-        	floor.setDownButtonPressed(true);
-        	floor.setUpButtonPressed(true);
-        	floors.add(floor);
-        }
-        var model = new EccModel(elevators, floors);
-        
-        
-		// init gui with model
-		var layout = new EccGuiLayout(model);
-		
+
+		controller.connect();
+		var model = controller.createModel();
+		controller.setModel(model);
+		controller.scheduleModelUpdater();
+
+		var layout = new EccGuiLayout(model, controller);
+
 		stage.setTitle("Elevator Control Center");
 		stage.setWidth(1500);
 	    stage.setHeight(800);
 	    stage.getIcons().add(new Image("icon.bmp"));
 		stage.setScene(layout.getScene());
+		stage.setOnCloseRequest(event -> controller.shutdownScheduler());
+
 	    stage.show();
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
