8556603116af681b16a648685adbbe5d6638c33f
MikeEdu <s2010567027@fhooe.at>
2022-01-19 01:05:36 +0100

    Add hack, to enable module-info and while tests are still working

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ecc.team.c {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.rmi;
+
+    exports at.fhhagenberg.sqe.ecc;
+    exports at.fhhagenberg.sqe.ecc.gui;
+    exports at.fhhagenberg.sqe.ecc.model;
+    exports sqelevator;
+}
